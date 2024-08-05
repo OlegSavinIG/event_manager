5cc2b47f28fdf78efc56f72e28896996d4151bef
Oleg <oleg211190gm@yandex.ru>
2024-08-05 20:28:17 +0300

    after review

--- ewm-service/src/main/java/ru/practicum/explorewithme/config/AppConfig.java
+++ ewm-service/src/main/java/ru/practicum/explorewithme/config/AppConfig.java
@@ -2,14 +2,15 @@ package ru.practicum.explorewithme.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * Configuration class for the application.
  */
 @Configuration
-@EnableTransactionManagement
 public class AppConfig {
 
     /**
@@ -20,5 +21,16 @@ public class AppConfig {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+    /**
+     * Creates and configures an ExecutorService bean.
+     *
+     * @return the configured ExecutorService
+     */
+    @Bean
+    public ExecutorService taskExecutor() {
+        int numCores = Runtime.getRuntime().availableProcessors();
+        int numThreads = numCores * 2;
+        return Executors.newFixedThreadPool(numThreads);
     }
 }
